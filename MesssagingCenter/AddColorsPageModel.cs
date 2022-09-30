9b667f3af0f9bfb6175bc96cb3cd598cfff5ebec
mahamat hammad <40486958+Mhamitay@users.noreply.github.com>
2022-09-30 02:17:40 -0500

    Init

---
+++ MesssagingCenter/AddColorsPageModel.cs
@@ -0,0 +1,33 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MesssagingCenter
+{
+    public class AddColorsPageModel : FreshBasePageModel
+    {
+     public ICommand AddColorsCommand { get; set; }
+     public string ColorName { get; set; }
+
+     public AddColorsPageModel()
+        {
+            AddColorsCommand = new Command(async () =>
+               await OnAddColors());
+        }
+
+     private async Task OnAddColors()
+        {
+            CustomColor color = new CustomColor();
+            color.ColorId = DateTime.Today.Ticks.ToString();
+            color.ColorName = ColorName;
+
+            MessagingCenter.Send<AddColorsPageModel,
+                  CustomColor>(this, "ColorUpdate", color);
+            await CoreMethods.PopToRoot(false);
+        }
+    }
+}
---
+++ MesssagingCenter/CustomColor.cs
@@ -0,0 +1,80 @@
+﻿using FreshMvvm;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace MesssagingCenter
+{
+    public class CustomColor
+    {
+        public string ColorId { get; set; }
+        public string ColorName { get; set; }
+    }
+
+    public class SampleAppPageModel : FreshBasePageModel,INotifyPropertyChanged
+    {
+        public ICommand GoToColorsCommand { get; set; }
+        ObservableCollection<CustomColor> _colorsList;
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public ObservableCollection<CustomColor> ColorsList
+        {
+            get { return _colorsList; }
+            set
+            {
+                _colorsList = value;
+            }
+        }
+
+        public SampleAppPageModel()
+        {
+            GoToColorsCommand = new Command(async () => await
+               OnNavigateDetailPage());
+        }
+        private async Task OnNavigateDetailPage()
+        {
+            await CoreMethods.PushPageModel<AddColorsPageModel>();
+        }
+        public override void Init(object initData)
+        {
+            ColorsList = new ObservableCollection<CustomColor>();
+
+            ColorsList.Add(new CustomColor
+            {
+                ColorId = "1",
+                ColorName = "Red"
+            });
+            ColorsList.Add(new CustomColor
+            {
+                ColorId = "2",
+                ColorName = "Green"
+            });
+            ColorsList.Add(new CustomColor
+            {
+                ColorId = "3",
+                ColorName = "Blue"
+            });
+
+
+
+
+            MessagingCenter.Subscribe<AddColorsPageModel,
+                  CustomColor>(this, "ColorUpdate",
+                     (page, updateColor) => {
+
+                         if (updateColor != null)
+                         {
+                             ColorsList.Add(updateColor);
+
+                         }
+                     });
+        }
+    }
+}
