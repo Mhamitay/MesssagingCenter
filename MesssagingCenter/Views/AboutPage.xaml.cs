115052ae7524ec506ccf9179c8bdc306c589eebd
mahamat hammad <40486958+Mhamitay@users.noreply.github.com>
2022-09-30 01:44:06 -0500

    Add project files.

---
+++ MesssagingCenter/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MesssagingCenter.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MesssagingCenter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MesssagingCenter.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MesssagingCenter.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
